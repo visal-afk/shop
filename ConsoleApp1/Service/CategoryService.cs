1e0776fcc492c7021fd23000f290ebc066646b30
Vusal Movsumlu <vusalmovsum@outlook.com>
2025-12-08 23:44:12 +0400

    new

--- ConsoleApp1/Service/CategoryService.cs
+++ ConsoleApp1/Service/CategoryService.cs
@@ -2,33 +2,29 @@
 using ConsoleApp1.exception;
 using ConsoleApp1.Models;
 using ConsoleApp1.Service;
+using ConsoleApp1.Service.Abstract;
+
 namespace ConsoleApp1.Services;
 
-public class CategoryService: BaseService
+public class CategoryService : BaseService, ICategoryService
 {
     public CategoryService(ShopDb shopDb) : base(shopDb)
     {
     }
-
-    public void AddCategory(Category category)
+    public void Add(Category item)
     {
-        var idCheck = _shopDb.categories.Any(c => c.Id == category.Id);
+        var idCheck = _shopDb.categories.Any(c => c.Id == item.Id);
         if (idCheck)
         {
             throw new Exception("Bu Id artiq movcuddur");
         }
-        _shopDb.categories.Add(category);
+        _shopDb.categories.Add(item);
     }
 
-    public List<Category> GetAllCategories()
+    public void Delete(int id)
     {
-        return _shopDb.categories;
-    }
-
-    public void RemoveCategory(int categoryId)
-    {
-        var category = _shopDb.categories.FirstOrDefault(c => c.Id == categoryId);
-
+        
+        var category = _shopDb.categories.FirstOrDefault(c => c.Id == id);
         if (category != null)
         {
             _shopDb.categories.Remove(category);
@@ -39,34 +35,39 @@ public class CategoryService: BaseService
         }
     }
 
-    public Category CategoryUpdate(Category category)
+    public List<Category> GetAll()
     {
-        var existingCategory = _shopDb.categories.FirstOrDefault(c => c.Id == category.Id);
-
-        if (existingCategory != null)
-        {
-            existingCategory.Name = category.Name;
-            existingCategory.Description = category.Description;
-            return existingCategory;
-        }
-        else
-        {
-            throw new NotFoundException("Bele category movcud deyil!");
-            
-        }
+        return _shopDb.categories;
     }
 
-    public Category CategoryGetById(int categoryId)
+    public Category GetById(int id)
     {
-        var category = _shopDb.categories.FirstOrDefault(c => c.Id == categoryId);
-
+        
+        var category = _shopDb.categories.FirstOrDefault(c => c.Id == id);
         if (category != null)
         {
             return category;
         }
         else
         {
-            throw new NotFoundException("Bele category movcud deyil!"); 
+            throw new NotFoundException("Bele category movcud deyil!");
+        }
+
+    }
+
+    public Category Update(Category item)
+    {
+        
+        var existingCategory = _shopDb.categories.FirstOrDefault(c => c.Id == item.Id);
+        if (existingCategory != null)
+        {
+            existingCategory.Name = item.Name;
+            existingCategory.Description = item.Description;
+            return existingCategory;
+        }
+        else
+        {
+            throw new NotFoundException("Bele category movcud deyil!");
         }
     }
 }
