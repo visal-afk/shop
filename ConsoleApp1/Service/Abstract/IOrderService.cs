a206d516be4e06fa2233d5822a39d0202148f8f9
Vusal Movsumlu <vusalmovsum@outlook.com>
2025-12-08 23:37:03 +0400

    new

--- ConsoleApp1/Service/Abstract/IOrderService.cs
+++ ConsoleApp1/Service/Abstract/IOrderService.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ConsoleApp1.Models;
+using ConsoleApp1.Services.Abstract;
 
-namespace ConsoleApp1.Service.Abstract
+namespace ConsoleApp1.Service.Abstract;
+
+public interface IOrderService: IService<Order>
 {
-    interface IOrderService
-    {
-    }
 }
