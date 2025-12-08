7a46e53b62a52e291b5bbbc7648164dde61963ab
Vusal Movsumlu <vusalmovsum@outlook.com>
2025-12-08 23:45:18 +0400

    new

---
+++ ConsoleApp1/UnitOfWork/Abstract/IUnitOfWorks.cs
@@ -0,0 +1,9 @@
+﻿using ConsoleApp1.Service.Abstract;
+namespace ConsoleApp1.UnitOfWork.Abstract;
+
+public interface IUnitOfWorks
+{
+    ICategoryService Categories { get; }
+    IProductService Products { get; }
+    IOrderService Orders { get; }
+    }
