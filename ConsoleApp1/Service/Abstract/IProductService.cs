089dc2764de2e4dbd1db92b62d6535e4c903d702
Vusal Movsumlu <vusalmovsum@outlook.com>
2025-12-08 23:37:14 +0400

    new

--- ConsoleApp1/Service/Abstract/IProductService.cs
+++ ConsoleApp1/Service/Abstract/IProductService.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ConsoleApp1.Models;
+using ConsoleApp1.Services.Abstract;
 
-namespace ConsoleApp1.Service.Abstract
+namespace ConsoleApp1.Service.Abstract;
+
+public interface IProductService : IService<Product>
 {
-    interface IProductService
-    {
-    }
+
 }
