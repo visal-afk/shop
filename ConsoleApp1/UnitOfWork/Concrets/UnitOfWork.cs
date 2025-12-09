3aedb7ab9b156fcfecc09643574d44024b501693
Vusal Movsumlu <vusalmovsum@outlook.com>
2025-12-09 21:28:45 +0400

    new\

---
+++ ConsoleApp1/UnitOfWork/Concrets/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp1.Database;
+using ConsoleApp1.Service;
+using ConsoleApp1.Service.Abstract;
+using ConsoleApp1.Services;
+using ConsoleApp1.UnitOfWork.Abstract;
+
+namespace ConsoleApp2.UnitOfWork.Concrets;
+
+public class UnitOfWork : IUnitOfWorks
+{
+    private readonly ShopDb _shopDb;
+
+    public UnitOfWork(ShopDb shopDb)
+    {
+        _shopDb = shopDb;
+    }
+
+    public ICategoryService Categories => throw new NotImplementedException();
+
+    public IProductService Products => throw new NotImplementedException();
+
+    public IOrderService Orders => throw new NotImplementedException();
+}
