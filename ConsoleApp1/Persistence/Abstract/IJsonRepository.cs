abc7f420a4dfbca40576d5fc2b8a99cd74210c08
Mövsümlü Vüsal Vüqar <movsu_vv44@itstep.edu.az>
2025-12-14 21:39:32 +0400

    new

---
+++ ConsoleApp1/Persistence/Abstract/IJsonRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApp1.Persistence.Abstract;
+
+public interface IJsonRepository<T>
+{
+    void SaveData(List<T> items);
+    List<T> LoadData();
+}
---
+++ ConsoleApp1/Persistence/Concrete/JsonRepository.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApp1.Persistence.Abstract;
+using System.Text.Json;
+
+namespace ConsoleApp1.Persistence.Concrete;
+
+public class JsonRepository<T> : IJsonRepository<T>
+{
+    private readonly string _filePath;
+    public JsonRepository(string filePath)
+    {
+        _filePath = filePath;
+    }
+    public List<T> LoadData()
+    {
+        if (!File.Exists(_filePath))
+            return new List<T>();
+
+        var json = File.ReadAllText(_filePath);
+        return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+    }
+
+    public void SaveData(List<T> items)
+    {
+        var json = JsonSerializer.Serialize(items, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(_filePath, json);
+    }
+}
+
