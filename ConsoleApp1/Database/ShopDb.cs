6e3245b32a16417921df198fc80e3be5f6ec4406
Mövsümlü Vüsal Vüqar <movsu_vv44@itstep.edu.az>
2025-12-14 21:36:16 +0400

    new

--- ConsoleApp1/Database/ShopDb.cs
+++ ConsoleApp1/Database/ShopDb.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleApp1.Models;
+using ConsoleApp1.Persistence.Concrete;
 namespace ConsoleApp1.Database;
 
  public class ShopDb   
@@ -6,5 +7,27 @@ namespace ConsoleApp1.Database;
         public List<Product> products = new List<Product>();
         public List<Category> categories = new List<Category>();
         public List<Order> orders=new List<Order>();
- }
+    private readonly JsonRepository<Product> _productRepository;
+    private readonly JsonRepository<Category> _categoryRepository;
+    private readonly JsonRepository<Order> _orderRepository;
+    public ShopDb()
+    {
+        _productRepository = new JsonRepository<Product>("products.json");
+        _categoryRepository = new JsonRepository<Category>("categories.json");
+        _orderRepository = new JsonRepository<Order>("orders.json");
+
+        products = _productRepository.LoadData();
+        categories = _categoryRepository.LoadData();
+        orders = _orderRepository.LoadData();
+    }
+    public void SaveChanges()
+    {
+        _productRepository.SaveData(products);
+        _categoryRepository.SaveData(categories);
+        _orderRepository.SaveData(orders);
+    }
+
+
+
+}
 
